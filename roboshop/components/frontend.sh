025d67ad48ea6cdf2c90862a710dc2d57068d897
AbhishekMaraboina <abhishekdv20@gmail.com>
2021-12-09 17:32:03 +0530

    frontend file implementation

---
+++ roboshop/components/frontend.sh
@@ -0,0 +1,21 @@
+yum install nginx -y
+if [ $? -ne 0 ]; then
+  echo "Nginx Install Failed"
+  exit 1
+fi
+
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zi"
+if [ $? -ne 0 ]; then
+  echo "Download Frontend Failed"
+  exit 1
+fi
+
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-master static README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+systemctl enable nginx
+systemctl start nginx
