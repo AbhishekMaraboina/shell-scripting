30c644b40a37161dae22f4fbc456b42190194f6b
AbhishekMaraboina <abhishekdv20@gmail.com>
2021-12-10 08:05:07 +0530

    to practice fxns and dirs

--- test.sh
+++ test.sh
@@ -5,11 +5,24 @@
 #echo ${*}
 #echo ${#}
 
-if [ $1 != abhi ]; then
-  exit
-else
-  echo "${1}"
-fi
+#if [ $1 != abhi ]; then
+#  exit
+#else
+#  echo "${1}"
+#fi
 
 #shell-scripting Functions and Directories practice
-ls
+LOG_FILE=/tmp/test.log
+rm -f ${LOG_FILE}
+
+STAT_CHECK() {
+  if [ $1 -ne 0 ]; then
+    echo -e "\e[1;31m${2} - FAILED\e[0m"
+    ls &>>${LOG_FILE}
+    exit 1
+  else
+    echo -e "\e[1;32m${2} - SUCCESS\e[0m"
+  fi
+}
+
+STAT_CHECK $1 $2
